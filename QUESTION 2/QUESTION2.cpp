888ea478bf601577ea129b7ffb479ef3f0bfd379
m00n <167011226+m00n-the-helius@users.noreply.github.com>
2024-04-16 20:08:15 +0530

    Add files via upload

---
+++ QUESTION 2/QUESTION2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char a='A';
+cout<<"The ASCII value of " << a << " is " << int(a) <<endl;
+char u='U';
+cout<<"The ASCII value of " << u << " is " << int(u);
+return 0;
+
+
+
+
+
+}
