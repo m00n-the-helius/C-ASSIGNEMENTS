bb8c9930dd0740c6713c5e419ecff8febb7d67b1
m00n <167011226+m00n-the-helius@users.noreply.github.com>
2024-04-16 20:40:14 +0530

    Add files via upload

---
+++ QUESTION 4/Q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int n1;
+cout<<"number is ";
+cin >>n1;
+int product;
+product = n1*n1*n1;
+cout<<"The cube of your given number is = " << product;
+return 0;
+
+
+}
