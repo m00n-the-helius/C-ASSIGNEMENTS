bd4568df7ae1cb1745cf474a14b363bcdce865f3
m00n <167011226+m00n-the-helius@users.noreply.github.com>
2024-04-16 19:43:35 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+int n1, n2;
+cout<<"please provide two numbers to be multiplied: ";
+cin >>n1 >>n2;
+int product;
+product= n1*n2;
+cout<<"product= "<<product;
+return 0;
+}
