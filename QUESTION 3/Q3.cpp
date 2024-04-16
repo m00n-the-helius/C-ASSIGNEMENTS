7c8746d3c25dd6781ff94c40ab9bfd0a0033e3e8
m00n <167011226+m00n-the-helius@users.noreply.github.com>
2024-04-16 20:31:30 +0530

    Add files via upload

---
+++ QUESTION 3/Q3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n1, n2;
+cout<<"Length and breadth of rectangle is ";
+cin >>n1 >>n2;
+int product;
+product = n1*n2;
+cout<<"The are of your given rectangle is = " <<product;
+return product;
+}
